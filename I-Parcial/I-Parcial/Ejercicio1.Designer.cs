6db10c3b5ffeab1ed731f7f66871a0c8e76328ac
HelenMartinez <hm586313@gmail.com>
2023-02-03 10:18:33 -0600

    Commit
    Primer  Commit

--- I-Parcial/I-Parcial/Ejercicio1.Designer.cs
+++ I-Parcial/I-Parcial/Ejercicio1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace I_Parcial
 {
-    partial class Form1
+    partial class Ejercicio1
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -29,13 +29,79 @@ namespace I_Parcial
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Numero1textBox = new System.Windows.Forms.TextBox();
+            this.Numero2textBox = new System.Windows.Forms.TextBox();
+            this.SumarButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numero 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numero 2:";
+            // 
+            // Numero1textBox
+            // 
+            this.Numero1textBox.Location = new System.Drawing.Point(166, 40);
+            this.Numero1textBox.Name = "Numero1textBox";
+            this.Numero1textBox.Size = new System.Drawing.Size(205, 22);
+            this.Numero1textBox.TabIndex = 2;
+            // 
+            // Numero2textBox
+            // 
+            this.Numero2textBox.Location = new System.Drawing.Point(166, 84);
+            this.Numero2textBox.Name = "Numero2textBox";
+            this.Numero2textBox.Size = new System.Drawing.Size(205, 22);
+            this.Numero2textBox.TabIndex = 3;
+            // 
+            // SumarButton
+            // 
+            this.SumarButton.Location = new System.Drawing.Point(179, 165);
+            this.SumarButton.Name = "SumarButton";
+            this.SumarButton.Size = new System.Drawing.Size(176, 46);
+            this.SumarButton.TabIndex = 4;
+            this.SumarButton.Text = "Sumar";
+            this.SumarButton.UseVisualStyleBackColor = true;
+            this.SumarButton.Click += new System.EventHandler(this.SumarButton_Click);
+            // 
+            // Ejercicio1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(700, 345);
+            this.Controls.Add(this.SumarButton);
+            this.Controls.Add(this.Numero2textBox);
+            this.Controls.Add(this.Numero1textBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Ejercicio1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Numero1textBox;
+        private System.Windows.Forms.TextBox Numero2textBox;
+        private System.Windows.Forms.Button SumarButton;
     }
 }
 
